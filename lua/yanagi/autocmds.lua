d529e1d2ef8755bd4723889823662dea860bf8ca
Yanagi57 <yanagisen@outlook.com>
2026-04-29 11:23:23 +0800

    Add trim trailing spaces on save

--- lua/yanagi/autocmds.lua
+++ lua/yanagi/autocmds.lua
@@ -6,3 +6,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
